f22002787d6cee399e2851e0319bc9b105da79fc
pkyoujin <yjin1435@gmail.com>
2017-06-21 22:20:11 +0900

    add OR JAVA CODE

--- app/src/main/java/mobile/andorid/yjin/op_research_ver2/GeneralActivity.java
+++ app/src/main/java/mobile/andorid/yjin/op_research_ver2/GeneralActivity.java
@@ -7,21 +7,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
 
-import java.net.URLEncoder;
-
-import static mobile.andorid.yjin.op_research_ver2.R.id.sign_table;
 import static mobile.andorid.yjin.op_research_ver2.R.id.table;
+
+//import static mobile.andorid.yjin.op_research_ver2.R.id.sign_table;
 
 /**
  * Created by You Jin on 2017-05-14.
@@ -33,7 +32,13 @@ public class GeneralActivity extends AppCompatActivity {
 
     private int selectedID = -1;
 
+    Spinner spinner;
+
     int x, y, v=0;
+    int vars;
+    int rows;
+    EditText[] arrEditText;
+    Spinner[] arrSpinner;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,23 +69,24 @@ public class GeneralActivity extends AppCompatActivity {
             }
         });
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_goal);
+        spinner = (Spinner) findViewById(R.id.spinner_goal);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.goal_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
         Intent i = getIntent();
-        int vars = i.getIntExtra("NUM_OF_VAR", 3);
-        int rows = i.getIntExtra("NUM_OF_ROW", 4);
+        vars = i.getIntExtra("NUM_OF_VAR", 3);
+        rows = i.getIntExtra("NUM_OF_ROW", 4);
+        arrEditText = new EditText[vars*rows];
+        arrSpinner = new Spinner[rows];
         Toast.makeText(getApplicationContext(), "vars=" + vars + ", rows=" + rows, Toast.LENGTH_SHORT).show();
 
-        TableLayout tl = (TableLayout) findViewById(table);
-        TableLayout t2 = (TableLayout) findViewById(sign_table);
+        TableLayout t1 = (TableLayout) findViewById(table);
+//        TableLayout t2 = (TableLayout) findViewById(sign_table);
         EditText et;
 
         for (x = 0; x < rows; x++) {
-
             TableRow row = new TableRow(this);
             TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
             row.setLayoutParams(lp);
@@ -89,26 +95,35 @@ public class GeneralActivity extends AppCompatActivity {
                 et = new EditText(this);
                 et.setHint("상수");
                 et.setId(v);
+                arrEditText[v] = et;
                 row.addView(et);
                 v++;
             }
-            tl.addView(row, x);
-        }
 
-        for (int z = 0; z < rows; z++) {
-
-            TableRow cons = new TableRow(this);
-            TableRow.LayoutParams cp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-            cons.setLayoutParams(cp);
-
-            Spinner constraints = (Spinner) findViewById(R.id.constraint_range);
-            ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
-                    this, R.array.cons_array, android.R.layout.simple_spinner_item);
+            Spinner constraints = new Spinner(this);
+            ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.cons_array, android.R.layout.simple_spinner_item);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             constraints.setAdapter(adapter2);
 
-            t2.addView(cons, z);
+            arrSpinner[x] = constraints;
+            row.addView(constraints);
+            t1.addView(row, x);
         }
+
+//        TableRow cons = new TableRow(this);
+//        TableRow.LayoutParams cp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+//        cons.setLayoutParams(cp);
+//
+//        for (int z = 0; z < rows; z++) {
+////            Spinner constraints = (Spinner) findViewById(R.id.constraint_range);
+//            Spinner constraints = new Spinner(this);
+//            ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.cons_array, android.R.layout.simple_spinner_item);
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//            constraints.setAdapter(adapter2);
+//
+//            ((ViewGroup) t2.getParent()).removeAllViews();
+//            t2.addView(cons, z);
+//        }
     }
 
     @Override
@@ -147,13 +162,39 @@ public class GeneralActivity extends AppCompatActivity {
                 builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.wolframalpha.com/widget/widgetPopup.jsp?p=v&id=1e692c6f72587b2cbd3e7be018fd8960&title=Linear%20Programming%20Calculator&theme=blue"));
+                        Log.d("onClick()", "진입");
 
-                        try{
-                            int getv = (int)findViewById(R.id.v)
-                            String param = "value = " + URLEncoder.encode()
-                        }
+                        Log.d("onClick()", "MAX/MIN: " + spinner.getSelectedItem().toString());
 
+                        Log.d("onClick()", "arrSpinner.length: " + arrSpinner.length);
+                        for(int i=0; i<arrSpinner.length; i++)
+                            Log.d("onClick()", "arrSpinner["+i+"]: " + arrSpinner[i].getSelectedItem().toString());
+
+                        Log.d("onClick()", "arrEditText.length: " + arrEditText.length);
+                        for(int i=0; i<arrEditText.length; i++)
+                            Log.d("onClick()", "arrEditText["+i+"]: " + arrEditText[i].getText().toString());
+
+
+                        String strUriParams = "&i0=";
+//                        String[] iValues = new String[arr.length];
+
+                        if (spinner.getSelectedItem().equals("MAX"))
+                            strUriParams += "Maximize";
+                        else if (spinner.getSelectedItem().equals("MIN"))
+                            strUriParams += "Minimize";
+
+//                        for()
+
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.wolframalpha.com/widget/widgetPopup.jsp?p=v&id=1e692c6f72587b2cbd3e7be018fd8960&title=Linear%20Programming%20Calculator&theme=blue"));
+
+//                        for(int i= 0; i <= v; i++){
+//                           arr = new EditText[v];
+//                            arr[i] = (EditText)findViewById (i);
+//
+//                            try{
+//                                String param = "value = " + URLEncoder.encode(arr[i]+"x"+i)
+//                            }
+//                        }
                         startActivity(intent);
                     }
                 });
